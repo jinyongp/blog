59158c8394608a532222b532f6824040b7d1619e
jinyongp <dev.jinyongp@gmail.com>
2023-07-20 00:03:32 +0900

    feat: setup template structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "accent": "var(--accent)",
+      }
+    },
   },
   plugins: [],
 }
