bc611edd9dda88a68c460ad8f565c10086051332
jinyongp <jinyongp@users.noreply.github.com>
2023-07-20 07:57:12 +0000

    deploy: 94b7546a5c3b4ea895874c7020496451fcd42a67

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const scrollToTopButton = document.getElementById('js-scroll-back-to-top');
+
+  if (scrollToTopButton) {
+    scrollToTopButton.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 500) {
+        scrollToTopButton.classList.remove('hide');
+      } else if (window.scrollY < 100) {
+        scrollToTopButton.classList.add('hide');
+      }
+    });
+  }
+});
