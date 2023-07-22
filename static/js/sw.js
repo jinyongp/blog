176bbd65e29afabde835ff804fa3cb352979d796
jinyongp <dev.jinyongp@gmail.com>
2023-07-23 04:27:59 +0900

    feat: pwa service worker 오류 수정 및 아이콘 변경

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,18 +1,40 @@
-self.addEventListener("install", async (event) => {
-  console.log("[ServiceWorker] Install");
-  const cache = await caches.open("jinyongp.dev");
-  event.waitUntil(cache.addAll(["/", "/index.html", "/src/"]));
+const key = "jinyongp.dev";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      console.log("[ServiceWorker] Install");
+      const cache = await caches.open(key);
+      return cache.addAll(["/", "/index.html"]);
+    })()
+  );
 });
 
-self.addEventListener("fetch", async (event) => {
-  const { request } = event;
-  console.log(`[ServiceWorker] Fetching resource: ${request.url}`);
-  let response = await caches.match(request);
-  if (!response) {
-    response = await fetch(request);
-    console.log(`[ServiceWorker] Caching new resource: ${request.url}`);
-    const cache = await caches.open("jinyongp.dev");
-    cache.put(request, response.clone());
-  }
-  event.respondWith(response);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const { request } = event;
+      console.log(`[ServiceWorker] Fetching resource: ${request.url}`);
+      let response = await caches.match(request);
+      if (!response) {
+        response = await fetch(request);
+        console.log(`[ServiceWorker] Caching new resource: ${request.url}`);
+        const cache = await caches.open(key);
+        cache.put(request, response.clone());
+      }
+      return response;
+    })()
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      const promise = Promise.all(
+        keys.map((_key) => _key === key && caches.delete(key))
+      );
+      return promise;
+    })()
+  );
 });
