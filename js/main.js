398c6d3996351e6e6f3ea238bc3ae1da619556d1
jinyongp <jinyongp@users.noreply.github.com>
2023-08-13 11:53:02 +0000

    deploy: c6c3bca7c8fb4b482147c4a64b9ed17c3ffcadde

--- js/main.js
+++ js/main.js
@@ -1,15 +1,2 @@
-const scrollToTopButton = document.getElementById("js-scroll-back-to-top");
-
-if (scrollToTopButton) {
-  scrollToTopButton.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 500) {
-      scrollToTopButton.classList.remove("hide");
-    } else if (window.scrollY < 100) {
-      scrollToTopButton.classList.add("hide");
-    }
-  });
-}
+import "./scroll-to-top.js";
+import "./lightbox.js";
