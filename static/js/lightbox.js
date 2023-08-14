df5335f03e8363773e720feb54ec0c97c7e1efed
jinyongp <dev.jinyongp@gmail.com>
2023-08-14 23:17:43 +0900

    perf: script/css load 최적화

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,31 +1,29 @@
-const article = document.querySelector("article.content");
-if (article) {
+window.addEventListener("DOMContentLoaded", async () => {
+  const article = document.querySelector("article.content");
+  if (!article) return;
+
   const images = [...article.querySelectorAll("img")];
   const sources = images.map((image) => ({ src: image.src }));
 
-  const lightbox = await (async () => {
-    const link = "https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.8/";
-    const [{ default: PhotoSwipeLightbox }, { default: PhotoSwipe }] =
-      await Promise.all([
-        import(`${link}photoswipe-lightbox.esm.min.js`),
-        import(`${link}photoswipe.esm.min.js`),
-      ]);
+  const link = "https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.8";
+  const [{ default: PhotoSwipeLightbox }, { default: PhotoSwipe }] =
+    await Promise.all([
+      import(`${link}/photoswipe-lightbox.esm.min.js`),
+      import(`${link}/photoswipe.esm.min.js`),
+    ]);
 
-    const lightbox = new PhotoSwipeLightbox({
-      pswpModule: PhotoSwipe,
-      showHideAnimationType: "none",
-      bgOpacity: 0.8,
-    });
+  const lightbox = new PhotoSwipeLightbox({
+    pswpModule: PhotoSwipe,
+    showHideAnimationType: "none",
+    bgOpacity: 0.8,
+  });
 
-    lightbox.addFilter("useContentPlaceholder", () => false);
-
-    lightbox.init();
-    return lightbox;
-  })();
+  lightbox.addFilter("useContentPlaceholder", () => false);
+  lightbox.init();
 
   article.addEventListener("click", (event) => {
     if (event.target instanceof HTMLImageElement) {
       lightbox.loadAndOpen(images.indexOf(event.target), sources);
     }
   });
-}
+});
